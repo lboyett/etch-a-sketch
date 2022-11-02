3eeb3eb3507d379e4a001b110555d631d3fe0e6c
Luke Boyett <lboyett@gmail.com>
2022-11-02 09:02:10 +0100

    Resize grid to N x N square. Random colors

--- jscript.js
+++ jscript.js
@@ -1,6 +1,6 @@
 const body = document.querySelector('body');
 
-// create div containing button prompt and grids
+// create div containing prompt button and grids
 const bodyContent = document.createElement('div');
 bodyContent.classList.add('bodyContent');
 body.appendChild(bodyContent);
@@ -9,23 +9,27 @@ bodyContent.setAttribute('style','border: 2px solid pink');
 // create button prompt
 const button = document.createElement('button');
 button.classList.add('button');
-button.textContent = "Enter Number of Squares";
+button.textContent = "Enter Grid Size";
 bodyContent.appendChild(button);
 
-
+// change grid on button click
 let promptResponse = (() => {
     response = prompt('What grid size?');
     return response;
 });
 
-
 button.addEventListener('click', () => {  // call promptResponse function on click
     let gridSize = promptResponse();   // Need this return from this function for later in the code
+    if (gridSize > 100) {
+        alert('Must be less than 100');
+        return;
+    }
     removeGrid();
     console.log('SIZE: '+ gridSize);
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(${gridSize}, 1fr)`);
     changeGrid(gridSize);
+    changeColor();
 });
-
 
 // create gridContainer for gridSquares
 let gridContainer = document.createElement('div');
@@ -38,20 +42,18 @@ gridContainer.setAttribute('style', 'display: grid');
 gridContainer.setAttribute('style', 'grid-template-columns: repeat(4, 1fr)');
 
 // create grid squares and append to gridContainer
-
 changeGrid(); //initialize grid to default value gridSize = 4
 
 function changeGrid(gridSize=4) {
     for (i=0; i<(gridSize*gridSize); i++) {
         let gridSquare = document.createElement('div');
-        gridSquare.setAttribute('style','border: 1px solid blue');
-        gridSquare.textContent = i;
+        //gridSquare.setAttribute('style','border: 1px solid blue');
         gridSquare.classList.add('gridSquare');
         gridContainer.appendChild(gridSquare);
     };
 };
 
-// Remove grid
+// add remove grid function
 function removeGrid() {
     let remGrid = document.querySelectorAll('.gridSquare');
     for (i=0; i<remGrid.length; i++){
@@ -59,13 +61,17 @@ function removeGrid() {
     };
 };
 
-// addEventListener so that squares change color when hover
-let gridSquares = document.querySelectorAll('.gridSquare');
-gridSquares.forEach((gridSquare) => {
-    gridSquare.addEventListener('mouseover', (event) => {
-        event.target.style.background = 'blue';
-        setTimeout(() => {
-            event.target.style.background='';
-        } , 500);
+// add function to change square color when hover
+changeColor();
+function changeColor() {
+    let gridSquares = document.querySelectorAll('.gridSquare');
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.addEventListener('mouseover', (event) => {
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            event.target.style.background = '#'+randomColor;
+            setTimeout(() => {
+                event.target.style.background='';
+            } , 500);
+        });
     });
-});
+};
