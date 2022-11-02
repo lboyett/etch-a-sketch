d3baa22a6a7faf25c8a21ca2bdcdb2ba7ef2088f
Luke Boyett <lboyett@gmail.com>
2022-11-02 07:11:43 +0100

    Add button to change grid

--- jscript.js
+++ jscript.js
@@ -1,9 +1,36 @@
 const body = document.querySelector('body');
 
+// create div containing button prompt and grids
+const bodyContent = document.createElement('div');
+bodyContent.classList.add('bodyContent');
+body.appendChild(bodyContent);
+bodyContent.setAttribute('style','border: 2px solid pink');
+
+// create button prompt
+const button = document.createElement('button');
+button.classList.add('button');
+button.textContent = "Enter Number of Squares";
+bodyContent.appendChild(button);
+
+
+let promptResponse = (() => {
+    response = prompt('What grid size?');
+    return response;
+});
+
+
+button.addEventListener('click', () => {  // call promptResponse function on click
+    let gridSize = promptResponse();   // Need this return from this function for later in the code
+    removeGrid();
+    console.log('SIZE: '+ gridSize);
+    changeGrid(gridSize);
+});
+
+
 // create gridContainer for gridSquares
-const gridContainer = document.createElement('div');
+let gridContainer = document.createElement('div');
 gridContainer.classList.add('gridContainer');
-body.appendChild(gridContainer);
+bodyContent.appendChild(gridContainer);
 
 // set attributes for gridContainer to arrange squares in a grid
 gridContainer.setAttribute('style','border: 1px solid red');
@@ -11,12 +38,34 @@ gridContainer.setAttribute('style', 'display: grid');
 gridContainer.setAttribute('style', 'grid-template-columns: repeat(4, 1fr)');
 
 // create grid squares and append to gridContainer
-for (i=0; i<16; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.setAttribute('style','border: 1px solid blue');
-    gridSquare.textContent = i;
-    gridSquare.classList.add('gridSquare');
-    gridContainer.appendChild(gridSquare);
-}
 
-let numSquares = 16;
+changeGrid(); //initialize grid to default value gridSize = 4
+
+function changeGrid(gridSize=4) {
+    for (i=0; i<(gridSize*gridSize); i++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.setAttribute('style','border: 1px solid blue');
+        gridSquare.textContent = i;
+        gridSquare.classList.add('gridSquare');
+        gridContainer.appendChild(gridSquare);
+    };
+};
+
+// Remove grid
+function removeGrid() {
+    let remGrid = document.querySelectorAll('.gridSquare');
+    for (i=0; i<remGrid.length; i++){
+        remGrid[i].remove();
+    };
+};
+
+// addEventListener so that squares change color when hover
+let gridSquares = document.querySelectorAll('.gridSquare');
+gridSquares.forEach((gridSquare) => {
+    gridSquare.addEventListener('mouseover', (event) => {
+        event.target.style.background = 'blue';
+        setTimeout(() => {
+            event.target.style.background='';
+        } , 500);
+    });
+});
